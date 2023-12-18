73164201b66f555cc2f1c2446f736bcd5512661a
Clare <clare.oparo@student.moringaschool.com>
2023-12-18 19:38:28 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+findMatching = (driverNames, hyeKyo) => {
+    return driverNames.filter((song) => song.toLowerCase() === hyeKyo.toLowerCase());
+
+}
+
+fuzzyMatch = (driverNames, shishi) => {
+    return driverNames.filter(
+      (firstLetters) =>
+        firstLetters.toLowerCase().indexOf(shishi.toLowerCase()) === 0
+    );
+}
+  
+matchName = (driver, jolie) => {
+    return driver.filter((origin) => origin.name === jolie);
+}
